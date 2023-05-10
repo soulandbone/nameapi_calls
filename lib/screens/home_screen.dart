3ee934e11539b72bbbd4acbadc1bd7249e6a0468
SoulAndBone <soulandbone@gmail.com>
2023-05-10 22:01:25 +0300

    Working API with ListTile in ListView builder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +11,41 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> users = [];
+
+  final String url = 'https://randomuser.me/api/?results=10';
+  void fetchUsers() async {
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final json = jsonDecode(response.body);
+    setState(() {
+      users = json['results'];
+    });
+    print(json['results']);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Name API call')),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_downward),
+        onPressed: () => fetchUsers(),
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) => Card(
+          elevation: 5,
+          color: Colors.black38,
+          child: ListTile(
+            leading: CircleAvatar(
+                foregroundImage:
+                    NetworkImage(users[index]['picture']['medium'])),
+            title: Text(users[index]['name']['first']),
+            subtitle: Text(users[index]['email']),
+          ),
+        ),
+      ),
+    );
   }
 }
