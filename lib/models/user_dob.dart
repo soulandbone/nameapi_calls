358e57486f37ba4b942f07c936fbc07961e2f735
SoulAndBone <soulandbone@gmail.com>
2023-05-15 13:05:21 +0300

    added two new model classes, UserDob and UserLocation

---
+++ lib/models/user_dob.dart
@@ -0,0 +1,6 @@
+class UserDob {
+  final int age;
+  final DateTime date;
+
+  UserDob({required this.age, required this.date});
+}
---
+++ lib/models/user_location.dart
@@ -0,0 +1,39 @@
+class UserLocation {
+  final LocationStreet street;
+  final String city;
+  final String state;
+  final String country;
+  final String postcode;
+  final LocationCoordinates coordinates;
+  final LocationTimeZone timeZone;
+
+  UserLocation(
+      {required this.street,
+      required this.city,
+      required this.state,
+      required this.country,
+      required this.postcode,
+      required this.coordinates,
+      required this.timeZone});
+}
+
+class LocationStreet {
+  final int number;
+  final String name;
+
+  LocationStreet({required this.number, required this.name});
+}
+
+class LocationCoordinates {
+  final String latitude;
+  final String longitude;
+
+  LocationCoordinates({required this.latitude, required this.longitude});
+}
+
+class LocationTimeZone {
+  final String offset;
+  final String description;
+
+  LocationTimeZone({required this.offset, required this.description});
+}
